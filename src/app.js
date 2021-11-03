c5c13e83142960618167095b2b6de1adc2bfc732
Charlie Gleason <hi@charliegleason.com>
2021-11-03 10:26:05 +0000

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import '@salesforce-ux/c360-styling-hooks/dist/hooks.custom-props.css';
+import C360Card from '@salesforce-ux/c360-card/dist/card';
+window.customElements.define('c360-card', C360Card);
